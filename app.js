5930e8118a921af94ab7c8be9f2208c2660a5ae5
faisal <developerkabirfaisal@gmail.com>
2022-05-18 20:43:38 +0600

    All file added

---
+++ app.js
@@ -0,0 +1,58 @@
+
+
+// Creating an email pattern
+const emailOutput = 'faisalkabir846@gmail.com';
+let email = /^[a-z0-9]{3,}(?:(\.|-|_)[a-z0-9]{1,})?@[a-z1-9]{3,}\.[a-z]{2,5}$/;
+
+console.log(`
+    Email Pattern
+    --------------------------------------
+    ${emailOutput}  =  ${email.test(emailOutput)}
+`);
+
+
+// Creating a username pattern
+
+const username = 'devdloperfaisal.me';
+let userOutput = /^[a-z0-9]{3,}(?:[\.-_]{1}[a-z0-9]{2,})?$/;
+
+console.log(`
+    Username Pattern
+    --------------------------------------
+    @${username}  =  ${userOutput.test(username)}
+`);
+
+
+// Creating a Bangladeshi Phone Number pattern
+
+const phoneNumber = '01628564417';
+let phoneOut = /^(\+8801|008801|01)[0-9]{9}$/;
+
+console.log(`
+    phoneNumber Pattern
+    --------------------------------------
+    ${phoneNumber}  =  ${phoneOut.test(phoneNumber)}
+`);
+
+
+// Creating a password pattern 
+// here at leaset use a upper case letter, lower case later and a special character. Password must be 6 to 16 character.
+const password = 'Faisal@1971';
+let passOut = /^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[~!@#$%^&*()_+\.<>]).{6,16}$/;
+
+console.log(`
+    password Pattern
+    --------------------------------------
+    ${password} lenth ${password.length} =  ${passOut.test(password)} 
+`);
+
+// Creating a zipcode pattern 
+
+const zipcode = '4700';
+let zipcodeOut = /^[0-9]{4,5}$/;
+
+console.log(`
+    zipcode Pattern
+    --------------------------------------
+    ${zipcode}  =  ${zipcodeOut.test(zipcode)}
+`);
